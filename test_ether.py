2a9a1fc118ed4ec479d661530dbc6614c1fea176
Kuan-Hao Chen <35591407+nghdavid@users.noreply.github.com>
2023-11-26 09:39:45 +0800

    chore: Change print

--- test_ether.py
+++ test_ether.py
@@ -11,14 +11,14 @@ load_dotenv()
 api_key = os.environ.get('API_KEY')
 tron_api_key = os.environ.get('TRON_API_KEY')
 total_balance = 0
-for address in list(EthWallet.wallet.keys()):
-    # print("It is: "+EthWallet.wallet.get(address))
-    for contract_address in list(EthWallet.contract_address.keys()):
-        url = f"https://{EthWallet.scan_domain}/api?module=account&action=tokenbalance&contractaddress={contract_address}&address={address}&tag=latest&apikey={api_key}"
-        response = requests.request("GET", url)
-        amount = int(int(json.loads(response.text).get("result"))*EthWallet.ratio)
-        # print(EthWallet.contract_address.get(contract_address)+" "+str(amount))
-        total_balance += amount
+# for address in list(EthWallet.wallet.keys()):
+#     # print("It is: "+EthWallet.wallet.get(address))
+#     for contract_address in list(EthWallet.contract_address.keys()):
+#         url = f"https://{EthWallet.scan_domain}/api?module=account&action=tokenbalance&contractaddress={contract_address}&address={address}&tag=latest&apikey={api_key}"
+#         response = requests.request("GET", url)
+#         amount = int(int(json.loads(response.text).get("result"))*EthWallet.ratio)
+#         # print(EthWallet.contract_address.get(contract_address)+" "+str(amount))
+#         total_balance += amount
 
 headers = {
   'TRON-PRO-API-KEY': tron_api_key
@@ -29,7 +29,7 @@ for address in list(TronWallet.wallet.keys()):
     for contract_address in list(TronWallet.contract_address.keys()):
         url = f"https://{TronWallet.scan_domain}/api/account/tokens?&address={address}"
         response = requests.request("GET", url, headers=headers)
-        print(json.loads(response.text))
+        print(response)
         data_list = json.loads(response.text).get("data")
         for data in data_list:
             if data.get("tokenId") == contract_address:
